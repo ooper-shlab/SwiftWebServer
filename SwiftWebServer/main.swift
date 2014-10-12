c913c6564f31dfe9501b789e3cb1ff08b5d7dcf7
開発 <dev@OOPers-MacBook.local>
2014-10-12 20:18:44 +0900

    Initial Commit

---
+++ SwiftWebServer/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SwiftWebServer
+//
+//  Created by 開発 on 2014/10/12.
+//  Copyright (c) 2014年 nagata_kobo. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
